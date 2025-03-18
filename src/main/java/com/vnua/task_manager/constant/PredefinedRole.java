32ef753bf7a58f5a1e7465aca90021b4679038d8
Kieuhieu2 <kieuhieu2@Kieuhieu2.local>
2025-03-18 21:49:48 +0700

    init project

--- src/main/java/com/vnua/task_manager/constant/PredefinedRole.java
+++ src/main/java/com/vnua/task_manager/constant/PredefinedRole.java
@@ -3,6 +3,7 @@ package com.vnua.task_manager.constant;
 public class PredefinedRole {
     public static final String USER_ROLE = "USER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String HEAD_OF_FACULTY_ROLE  = "HEAD_OF_FACULTY_ROLE";
 
     private PredefinedRole() {}
 }
